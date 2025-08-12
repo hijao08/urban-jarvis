620d296f0ca35e038f66f0bd1007b00d70866325
João Vitor Soares Da Silva <jvsoares082004@gmail.com>
2025-08-12 18:43:40 -0300

    Atualiza dependências do projeto, incluindo a versão do Express para 4.18.2 e ajustes no arquivo server.js para simplificar a configuração do servidor. Modificações no package.json e package-lock.json para refletir as novas versões das dependências.

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Servir arquivos estáticos da pasta build
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static('build'));
 
-// Todas as requisições não atendidas pelos arquivos estáticos serão direcionadas para o index.html
-app.get('*', (req, res) => {
+// Rota para todas as outras requisições
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
